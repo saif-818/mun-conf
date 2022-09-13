c6e4b322e9a81439623c22b5de660fd3ae094011
Saifuddin <85546997+saif-818@users.noreply.github.com>
2022-09-13 22:59:39 +0530

    Updated site

--- src/components/CitiesSlider.js
+++ src/components/CitiesSlider.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import "./CitiesSlider.scss";
 import classNames from "classnames";
+import EventIcon from '@mui/icons-material/Event';
+import PlaceIcon from '@mui/icons-material/Place';
 
 const CitiesSlider = (props) => {
   let IMAGE_PARTS = 4;
@@ -40,7 +42,7 @@ const CitiesSlider = (props) => {
 
   return (
     <div className={classNames("slider", { "s--ready": sliderReady })}>
-      {/* <p className="slider__top-heading">Conference</p> */}
+      {/* <p className="slider__top-heading"></p> */}
       <div className="slider__slides">
         {props.slides.map((slide, index) => (
           <div
@@ -60,8 +62,18 @@ const CitiesSlider = (props) => {
                 ))} */}
                 {'-Think,Dicuss,Prosper-'}
               </h4>
-              <h3 className='event-schedule' style={{marginBottom:'5px',fontWeight:'200'}}>30th April and 1st May 2022</h3>
-              <a className="slider__slide-readmore" href={'https://linktr.ee/pictmun'} style={{color: 'white',textDecoration: 'none', fontSize: '15px'}}>Register here</a>
+              {/* <h3 className='event-schedule' style={{marginBottom:'5px',fontWeight:'200'}}>30th April and 1st May 2022</h3> */}
+              <div className="timeline_location" style={{display: 'flex',justifyContent: 'space-evenly',alignItems: 'center',margin: '20px'}}>
+                <div className="sub_heading1" style={{display: 'flex'}}>
+                  <div> <EventIcon/> </div>
+                  <div style={{marginLeft: '5px'}}>October 15<sup>th</sup> and 16<sup>th</sup>, 2022</div>
+                </div>
+                <div className="sub_heading2" style={{display: 'flex'}}>
+                  <div style={{marginLeft: '15px'}}> <PlaceIcon/> </div>
+                  <div style={{marginLeft: '5px',marginTop: '3px'}}> PICT Pune </div>
+                </div>
+             </div>
+              <a href={'https://linktr.ee/pictmun'} style={{color: 'white',textDecoration: 'none', fontSize: '15px', padding: '10px',border: '2px solid white'}}>Register here</a>
             </div>
             
             <div className="slider__slide-parts">
--- src/components/Stats.js
+++ src/components/Stats.js
@@ -4,7 +4,7 @@ const Stats = () => {
   return (
     <div class="stats">
                 <div class="stats__item">
-                  <p class="stats__num"><span>2</span>days</p>
+                  <p class="stats__num"><span>12+</span>hours</p>
                 </div>
                 <div class="stats__item">
                   <p class="stats__num"><span>6+</span>delegates</p>
